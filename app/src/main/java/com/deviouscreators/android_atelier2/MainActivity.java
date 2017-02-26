126828aa3d8136643ba9226945bcaa47fe7ddaf2
nguyeale <nguyeale@uqtr.ca>
2017-02-26 15:30:49 -0400

    Initialisation of a new Android project

---
+++ app/src/main/java/com/deviouscreators/android_atelier2/MainActivity.java
@@ -0,0 +1,13 @@
+package com.deviouscreators.android_atelier2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
